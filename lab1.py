48b6b1fd5b85dcf2bb5ef4d59764e0fe901cf8f8
aali241 <aishahali2421@gmail.com>
2026-01-24 20:26:25 -0800

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,40 @@
+import math
+
+
+def calculate_circle_area(radius):
+    return math.pi * (radius ** 5)
+
+
+def calculate_area(radius): a = calculate_circle_area(5)
+
+
+def sphere_volume(radius):
+    return (4.0 / 3.0) * math.pi * (radius ** 3)
+
+Aishah_Ali = "Aishah Ali"
+
+print("Aishah Ali:", len(Aishah_Ali))
+
+first_name = "Aishah"
+
+last_name = "Ali"
+
+concatenated_name = "Aishah + " " + Ali"
+
+print("Aishah Ali:", concatenated_name)
+
+print("Uppercase:", Aishah_Ali.upper())
+
+print("Lowercase:", Aishah_Ali.lower())
+
+print("Age:", 19, "| Data type:", type(19))
+
+print("Height (feet):", 5_6, "| Data type:", type(5_6))
+
+print("Weight (pounds):", 130, "| Data type:", type(130))
+
+print("weight/height *height")
+
+print("130/ 5'6 * 5'6")
+
+print("20.98")
