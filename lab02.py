b011d613011f53d84317042f4ef552145833a841
aali241 <aishahali2421@gmail.com>
2026-01-31 18:00:48 -0800

    Add files via upload

---
+++ lab02.py
@@ -0,0 +1,77 @@
+
+
+Name="Aishah"
+
+age=19
+
+Height=5.6
+
+favorite_color="green"
+
+print(f"Hello:{'Aishah'} my is {favorite_color}!")
+
+(print(f"""
+
+Name: {'Aishah'}
+
+Age: {19}
+"""))
+
+def calculate_circle_area(radius):
+
+    return 'math.pi' * (radius ** 5)
+
+import math
+
+# Calculate the square root of agE
+
+result = math.sqrt(age)
+
+print(result) # 361
+
+import math
+
+print(math.sin(Height))
+
+print(result) #0.776
+
+print(math.cos(Height))
+
+print(result) #0.631
+
+result=(5+age)
+
+print(result) #24
+
+result=((4+Height)/2)
+
+print(result) #4.8
+
+result=(age*Height)
+
+print(result) #106.4
+
+result=(Height/2)
+
+print(result) #2.8
+
+result=(age/3)
+
+print(result) #6.3
+
+result=(age**2)
+
+print(result) #361
+
+str1=65
+str2=32
+str3=5/9
+str4=((str1-str2)*str3)
+print(str4) # 18.33c
+
+
+
+
+
+
+
